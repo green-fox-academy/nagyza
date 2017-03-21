d79c5c80629050444db3f7c964ad5b9415dceca2
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 22:04:59 +0100

    Scanner created and it's content added to a double variable.

---
+++ week-02/day-02/src/Exercise_15.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class Exercise_15 {
+  public static void main(String[] args) {
+    // Write a program that asks for an integer that is a distance in kilometers,
+    // then it converts that value to miles and prints it
+    //1 km is 0.621371192237 miles
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Add meg a tavolsagot km-ben! ");
+    double km = scanner.nextDouble();
+
+  }
+}
