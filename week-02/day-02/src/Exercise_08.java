c4710876c4adee983635ab40386aeaadbb7cc102
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 16:33:40 +0100

    Declared main variables, different types.

---
+++ week-02/day-02/src/Exercise_08.java
@@ -0,0 +1,12 @@
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class Exercise_08 {
+  public static void main(String[] args) {
+    String myName = "Zsolt Nagy";
+    int myAge = 43;
+    double myHeight = 1.73;
+    boolean isMarried = true;
+    
+  }
+}
