5d611e28e3b29bba8c03ae5bf9d9781ebfe5030a
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 20:26:30 +0100

    Calculated body mass index.

---
+++ week-02/day-02/src/Exercise_09.java
@@ -0,0 +1,10 @@
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class Exercise_09 {
+  public static void main(String[] args) {
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+    System.out.println("BMI is: " + (massInKg / (heightInM * heightInM)));
+  }
+}
