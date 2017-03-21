b2dec8af210ab32970245b34903724f8b58e03bc
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 16:46:58 +0100

    Declared variables for all 3 sides of the cuboid, and calculation of the surface area.

---
+++ week-02/day-02/src/Exercise_10.java
@@ -0,0 +1,12 @@
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class Exercise_10 {
+  public static void main(String[] args) {
+    int sideA = 12;
+    int sideB = 22;
+    int sideC = 13;
+    int surfaceArea = (sideA * sideB + sideA * sideC + sideB * sideC) * 2;
+    System.out.println("Surface Area: " + surfaceArea);
+  }
+}
