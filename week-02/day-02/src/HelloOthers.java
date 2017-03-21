aac306491c9c0be4919eefc1a3bc7afd7d0b4378
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 16:23:32 +0100

    Exercise_02 of the day-02, Hello, World to others version.

---
+++ week-02/day-02/src/HelloOthers.java
@@ -0,0 +1,10 @@
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class HelloOthers {
+  public static void main(String[] args) {
+    System.out.println("Hello, Vitya!");
+    System.out.println("Hello, Zsiguli!");
+    System.out.println("Hello, Virag!");
+  }
+}
