7b96e86cacda39ad14a7a57bce2e184768c816d7
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 17:15:12 +0100

    Declared 'a' and 'b' variable and a variable to hold temporary one of the 'a' or 'b'.

---
+++ week-02/day-02/src/Exercise_12.java
@@ -0,0 +1,18 @@
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class Exercise_12 {
+  public static void main(String[] args) {
+    int a = 123;
+    int b = 526;
+    int temp = a;
+    
+
+
+
+
+    System.out.println(a);
+    System.out.println(b);
+
+  }
+}
