d7a39dbc6a8b9190aa954918e4cd36f5920a3c7e
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 16:52:10 +0100

    Basic maths with two number.

---
+++ week-02/day-02/src/Exercise_05.java
@@ -0,0 +1,15 @@
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class Exercise_05 {
+  public static void main(String[] args) {
+    int numberOne = 22;
+    int numberTwo = 13;
+    System.out.println(numberTwo + numberOne);
+    System.out.println(numberOne - numberTwo);
+    System.out.println(numberOne * numberTwo);
+    System.out.println(((double) numberOne) / ((double) numberTwo));
+    System.out.println(numberOne / numberTwo);
+    System.out.println(numberOne % numberTwo);
+  }
+}
