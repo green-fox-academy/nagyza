006c866e800d07ed03ec8cb3273a09e8318882ac
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 17:23:02 +0100

    Print out a message with an integer in a line together

---
+++ week-02/day-02/src/Exercise_07.java
@@ -0,0 +1,9 @@
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class Exercise_07 {
+  public static void main(String[] args) {
+    int favourite_number = 8;
+    System.out.println("My favourite number is: " + favourite_number);
+  }
+}
