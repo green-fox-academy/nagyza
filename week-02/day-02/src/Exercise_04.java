69bacab90be3565787ff5fe47fdd236d96c3cdd5
Zsolt Nagy <nagyza@gmail.com>
2017-03-21 16:26:50 +0100

    Exercise_04 of the day-02, printing some personal data to command line.

---
+++ week-02/day-02/src/Exercise_04.java
@@ -0,0 +1,10 @@
+/**
+ * Created by nagyza on 2017.03.21..
+ */
+public class Exercise_04 {
+  public static void main(String[] args) {
+    System.out.println("Zsolt Nagy");
+    System.out.println(43);
+    System.out.println(1.73);
+  }
+}
