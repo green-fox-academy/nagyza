6427a020cc1a60ca977b12321be855040d70d489
Zsolt Nagy <nagyza@gmail.com>
2017-03-22 15:24:13 +0100

    Renamed 'factorio' function to 'Factorio', works well, println-s deleted.

--- week-02/day-03/src/Factorial.java
+++ week-02/day-03/src/Factorial.java
@@ -7,15 +7,13 @@ public class Factorial {
     //  and main method on your own.
     // - Create a function called `factorio`
     //   that returns it's input's factorial
-    System.out.println(Factorio(3));
+    System.out.println(Factorio(6));
   }
 
   public static int Factorio(int number) {
     int fakt = 1;
     for (int i = 0; i < number; i++) {
       fakt *= i + 1;
-      System.out.println("i-" + (i + 1));
-      System.out.println(fakt);
     }
     return fakt;
   }
