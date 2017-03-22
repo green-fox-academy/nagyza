cb52a5d99148bee32581507b65a21c53dcbf56cf
Zsolt Nagy <nagyza@gmail.com>
2017-03-22 09:52:55 +0100

    Usage of a 'for' statement to repeat a message (or any else task).

---
+++ week-02/day-02/src/Exercise_23.java
@@ -0,0 +1,12 @@
+/**
+ * Created by nagyza on 2017.03.22..
+ */
+public class Exercise_23 {
+  public static void main(String[] args) {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+    for (int i = 0; i < 100; i++) {
+      System.out.println("I won't cheat on the exam!" + i); // 'i' is to count to see what happens.
+    }
+  }
+}
