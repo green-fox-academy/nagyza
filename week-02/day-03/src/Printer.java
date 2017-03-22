bc9de18619ae0259ee067700a745ccb705824768
Zsolt Nagy <nagyza@gmail.com>
2017-03-22 23:26:19 +0100

    Function print any number of String parameter.

---
+++ week-02/day-03/src/Printer.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * Created by nagyza on 2017.03.22..
+ */
+public class Printer {
+  public static void main(String[] args) {
+    //  Create the usual class wrapper
+    //  and main method on your own.
+
+    // - Create a function called `printer`
+    //   which prints the input String parameters
+    // - It can have any number of parameters
+
+    // Examples
+    // printer("first")
+    // printer("first", "second")
+    // printer("first", "second", "third", "fourh")
+    // ...
+
+    printer("first");
+    printer("first", "second");
+    printer("first", "second", "third", "fourh");
+    printer("alma", "banan");
+  }
+
+  public static void printer(String ... text) {
+    for (String stringParameter : text) {
+      System.out.println(stringParameter);
+    }
+  }
+}
