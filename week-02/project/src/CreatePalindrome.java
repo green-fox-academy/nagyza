2c4fcf3f9c52ebaa066b406775d564c8a8c781ce
Zsolt Nagy <nagyza@gmail.com>
2017-03-24 11:05:07 +0100

    Initialise scanner.

---
+++ week-02/project/src/CreatePalindrome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by nagyza on 2017.03.24..
+ */
+public class CreatePalindrome {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Type a text! I'll build a palindrome from that. ");
+    String typedString = scanner.nextLine();
+
+  }
+}
