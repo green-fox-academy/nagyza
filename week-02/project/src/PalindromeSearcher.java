ee82bfa31d70ba5cd2116a0f76a959f1182b26ea
Zsolt Nagy <nagyza@gmail.com>
2017-03-24 13:05:32 +0100

    Simplify control flow.

--- week-02/project/src/PalindromeSearcher.java
+++ week-02/project/src/PalindromeSearcher.java
@@ -8,28 +8,23 @@ import java.util.Arrays;
  */
 public class PalindromeSearcher {
   public static void main(String[] args) {
-    ArrayList<String> foundPalindromes = new ArrayList<>(); // todo probably indicate <String>
+    ArrayList<String> foundPalindromes = new ArrayList<>();
     String stringToExamine = "dog goat dad duck doodle never";
-    for (int i = 3; i < stringToExamine.length() - i; i++) {
-
-
+    for (int i = stringToExamine.length(); i > 2; i--) { // todo egybetuseket kizarni
+      for (int  j = 0; j < i + 1; j++) {
+        if (palTrial(stringToExamine.substring(j, i))) {
+          System.out.println("j " + j + "i" + i + "bent " + stringToExamine.substring(j, i));
+          foundPalindromes.add(stringToExamine.substring(j, i));
+        }
+      }
     }
-
-  }
-  private static ArrayList<String> examineString(String stringToEx, int palLen) {
-    String palTry = "";
-    for (int i = 0; i < stringToEx.length() - palLen; i++) { // todo lehet hogy a kozepen jatszani kell
-      palTry = stringToEx.substring(i, (i + palLen);
-
-    }
+    System.out.println(foundPalindromes);
   }
 
   private static boolean palTrial(String palTry) {
     for (int j = 0; j < (palTry.length()/2); j++) {
-      System.out.println("j " + j + " len/2 " + (palTry.length()/2));
       int charNum = j;
       if (palTry.charAt(charNum) != palTry.charAt((palTry.length() - 1) - charNum)) {
-        System.out.println((palTry.charAt(charNum)) + " " + (palTry.charAt((palTry.length() - 1) - charNum)));
         return false;
       }
     }
