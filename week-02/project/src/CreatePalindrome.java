c550f0673d4d014d0557c440088a0b36084acebb
Zsolt Nagy <nagyza@gmail.com>
2017-03-24 11:28:11 +0100

    Make a String backward then it is was in the Scanner  and print out concatenated to the Scanner content.

--- week-02/project/src/CreatePalindrome.java
+++ week-02/project/src/CreatePalindrome.java
@@ -8,6 +8,11 @@ public class CreatePalindrome {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Type a text! I'll build a palindrome from that. ");
     String typedString = scanner.nextLine();
-
+    String typedBack = "";
+    for (int i = typedString.length() - 1; i > -1; i--) {
+      Character current = typedString.charAt(i);
+      typedBack = typedBack.concat(current.toString());
+    }
+    System.out.println("The palindrome I made is: " +(typedString + typedBack));
   }
 }
