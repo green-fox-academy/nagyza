6af991845c715a2059d028cd36eab1d7f71f8810
Zsolt Nagy <nagyza@gmail.com>
2017-03-27 14:42:00 +0200

    Function remove doubled characters from a String line read from a txt file.

---
+++ week-03/day-01/src/Doubled.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+/**
+ * Created by nagyza on 2017.03.27..
+ */
+public class Doubled {
+  public static void main(String[] args) {
+    // Create a method that decrypts the assets/duplicated_chars.txt
+    Path filePath = Paths.get("duplicated-chars.txt");
+    StringBuilder normalLine = new StringBuilder();
+    try {
+      ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(filePath);
+      for (String line : lines) {
+        normalLine.append(normLines(line));
+      }
+    } catch (IOException ex) {
+      ex.getMessage();
+    }
+    System.out.println(normalLine);
+  }
+
+  public static StringBuilder normLines(String line) {
+    ArrayList<Character> characters = new ArrayList<Character>();
+    StringBuilder normLine = new StringBuilder();
+    for (int i = 0; i < line.length(); i++) {
+      characters.add(i, line.charAt(i));
+    }
+    for (int i = 0; i < characters.size(); i++) {
+      if (characters.get(i) == characters.get(i + 1)) {
+        characters.remove(i + 1);
+      }
+    }
+    for (int i = 0; i < characters.size(); i++) {
+      normLine.append(characters.get(i));
+    }
+    normLine = normLine.append("\n");
+    return normLine;
+  }
+}
