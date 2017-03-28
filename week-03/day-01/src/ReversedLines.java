d5f17a1661f234c44863768c3d7042364671b4b9
Zsolt Nagy <nagyza@gmail.com>
2017-03-28 21:31:55 +0200

    Write content to file by main function.
    Fixed function fills ArrayList with the expected content.

--- week-03/day-01/src/ReversedLines.java
+++ week-03/day-01/src/ReversedLines.java
@@ -10,23 +10,25 @@ import java.util.List;
  */
 public class ReversedLines {
   final public static String READ_PATH = "reversed-lines.txt";
-  final public static String WRITE_PATH = "rereversed-lines.txt";
+  final public static String WRITE_PATH = "re-reversed-lines.txt";
 
   public static void main(String[] args) {
-    readLines(readFile());
+    try {
+      Path pathToWrite = Paths.get(WRITE_PATH);
+      Files.write(pathToWrite, readLines(readFile()));
+    } catch (IOException ex) {
+      System.out.println("Something wrong with writing file.");
+    }
   }
-
-
 
   public static ArrayList<StringBuilder> readLines(List<String> linesFromFile) {
     ArrayList<StringBuilder> lines = new ArrayList<>();
-    StringBuilder linesReverse = new StringBuilder();
-    for (int i =0; i < linesFromFile.size(); i++) {
-      linesReverse.append(linesFromFile.get(i));
-      linesReverse.reverse();
-      lines.add(linesReverse);
-      System.out.println(linesReverse);
-      linesReverse.setLength(0);
+    for (String line : linesFromFile) {
+      StringBuilder lineReverse = new StringBuilder();
+      lineReverse.append(line);
+      lineReverse.reverse();
+      lines.add(lineReverse);
+      System.out.println(lineReverse);
     }
     return lines;
   }
@@ -37,9 +39,8 @@ public class ReversedLines {
       Path filePath = Paths.get(READ_PATH);
       lines = Files.readAllLines(filePath);
     } catch (IOException e) {
-      System.out.println("Something wrong with read file.");
+      System.out.println("Something wrong with reading file.");
     }
-    System.out.println("ok");
     return lines;
   }
 }
