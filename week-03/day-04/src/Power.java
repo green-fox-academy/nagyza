e8f19c208ad7fbcdf4a4758186511fd95b0e4650
Zsolt Nagy <nagyza@gmail.com>
2017-03-30 11:13:34 +0200

    Fixed to be able to handle base on 0 power.

--- week-03/day-04/src/Power.java
+++ week-03/day-04/src/Power.java
@@ -6,12 +6,12 @@ public class Power {
     // Given base and n that are both 1 or more, compute recursively (no loops)
     // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 
-    System.out.println(power(2,3));
+    System.out.println(power(3,1));
   }
 
   public static int power(int base, int n) {
-    if (n == 1) {
-      return base;
+    if (n == 0) {
+      return 1;
     } else {
       return base * power(base, n - 1);
     }
