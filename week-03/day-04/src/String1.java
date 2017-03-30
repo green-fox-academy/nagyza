8adf150aa1a123343df8f13bbfd2c5e13e1c0621
Zsolt Nagy <nagyza@gmail.com>
2017-03-30 11:58:30 +0200

    Fix style of code.

--- week-03/day-04/src/String1.java
+++ week-03/day-04/src/String1.java
@@ -6,7 +6,6 @@ public class String1 {
     // Given a string, compute recursively (no loops) a new string where all the
     // lowercase 'x' chars have been changed to 'y' chars.
     System.out.println(changeChars("valxax kjhkasx"));
-
   }
 
   public static String changeChars(String text) {
@@ -18,6 +17,4 @@ public class String1 {
       return changeChars(text.substring(0, index) + "y" + text.substring(index + 1, text.length()));
     }
   }
-
-
 }
