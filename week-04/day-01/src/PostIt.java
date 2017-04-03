52bfef1f22c0d67272432de1a831f912e38cfc7e
Zsolt Nagy <nagyza@gmail.com>
2017-04-03 11:08:55 +0200

    Class with 3 variables and two constructor.

---
+++ week-04/day-01/src/PostIt.java
@@ -0,0 +1,17 @@
+/**
+ * Created by nagyza on 2017.04.03..
+ */
+class PostIt {
+  String backgroundColor;
+  String text;
+  String textColor;
+
+  public PostIt() {
+  }
+
+  public PostIt(String backgroundColor, String text, String textColor) {
+    this.backgroundColor = backgroundColor;
+    this.text = text;
+    this.textColor = textColor;
+  }
+}
