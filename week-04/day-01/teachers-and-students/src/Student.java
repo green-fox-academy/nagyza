c0290419d4287e7972888ec0caab591cd3b4292a
Zsolt Nagy <nagyza@gmail.com>
2017-04-03 17:42:34 +0200

    Create toString method and complete main method.

--- week-04/day-01/teachers-and-students/src/Student.java
+++ week-04/day-01/teachers-and-students/src/Student.java
@@ -14,7 +14,21 @@ public class Student {
     this.knowledge++;
   }
 
+  @Override
+  public String toString() {
+    return  "name='" + name + '\'' +
+            ", knowledge=" + knowledge +
+            '}';
+  }
+
   public String question(Teacher teacher, String questionToAsk) {
     return teacher.answer(questionToAsk);
+  }
+
+  public static void main(String[] args) {
+    Teacher teacher1 = new Teacher("Marika");
+    Student student1 = new Student("Marci");
+    System.out.println(student1.question(teacher1, "Is that method works well?"));
+    teacher1.teach(student1);
   }
 }
