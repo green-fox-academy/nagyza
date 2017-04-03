1fa169a8ee2b2eb4fb68e35c0c801e46a18dad0a
Zsolt Nagy <nagyza@gmail.com>
2017-04-03 13:58:39 +0200

    Method to sort dominoes.

--- week-04/day-01/dominoes/src/Dominoes.java
+++ week-04/day-01/dominoes/src/Dominoes.java
@@ -11,8 +11,21 @@ public class Dominoes {
     // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
     // eg: [2, 4], [4, 3], [3, 5] ...
     initializeDominoes();
-
     System.out.println(dominoes);
+    System.out.println(sortDominoes(initializeDominoes()));
+  }
+
+  public static List<Domino> sortDominoes(List<Domino> dominoes) {
+    for (int j = 1; j < dominoes.size(); j++) {
+      for (int i = 1; i < dominoes.size(); i++) {
+        if (dominoes.get(i - 1).getValues()[1] != dominoes.get(i).getValues()[0]) {
+          Domino keepDomino = dominoes.get(i);
+          dominoes.remove(i);
+          dominoes.add(keepDomino);
+        }
+      }
+    }
+    return dominoes;
   }
 
   static List<Domino> initializeDominoes() {
