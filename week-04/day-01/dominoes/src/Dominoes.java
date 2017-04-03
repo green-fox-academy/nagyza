4e633f0f7fb215baa40c87f32f94d02539b1d8fe
Zsolt Nagy <nagyza@gmail.com>
2017-04-03 14:00:22 +0200

    Fix for loop in sortDominoes method.

--- week-04/day-01/dominoes/src/Dominoes.java
+++ week-04/day-01/dominoes/src/Dominoes.java
@@ -16,7 +16,7 @@ public class Dominoes {
   }
 
   public static List<Domino> sortDominoes(List<Domino> dominoes) {
-    for (int j = 1; j < dominoes.size(); j++) {
+    for (int j = 0; j < dominoes.size(); j++) {
       for (int i = 1; i < dominoes.size(); i++) {
         if (dominoes.get(i - 1).getValues()[1] != dominoes.get(i).getValues()[0]) {
           Domino keepDomino = dominoes.get(i);
