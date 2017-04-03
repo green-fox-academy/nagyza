dadf30ee2330080c44b3f7c6aad7eb2fd11f2860
Zsolt Nagy <nagyza@gmail.com>
2017-04-03 14:37:28 +0200

    Use methods of Dice type object.
    Roll first and reroll until all values are six.

--- week-04/day-01/dice/src/Dice.java
+++ week-04/day-01/dice/src/Dice.java
@@ -38,13 +38,15 @@ public class Dice {
 
   public static void main(String[] args) {
     Dice myDice = new Dice();
-    myDice.getCurrent();
     myDice.roll();
-    myDice.getCurrent();
-    myDice.getCurrent(5);
-    myDice.reroll();
-    myDice.getCurrent();
-    myDice.reroll(4);
-    myDice.getCurrent();
+
+    for (int i = 0; i < myDice.getCurrent().length; i++) {
+      while (myDice.getCurrent(i) != 6) {
+        myDice.reroll(i);
+      }
+    }
+    for (int i = 0; i < myDice.getCurrent().length; i++) {
+      System.out.println("Dice-" + i + ": " + myDice.getCurrent(i));
+    }
   }
 }
