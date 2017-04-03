c6509dba5503f35199c8231418595fb77540023b
Zsolt Nagy <nagyza@gmail.com>
2017-04-03 17:40:59 +0200

    Create constructor and new class variable.
    Complete answer method.

--- week-04/day-01/teachers-and-students/src/Teacher.java
+++ week-04/day-01/teachers-and-students/src/Teacher.java
@@ -3,16 +3,33 @@
  */
 public class Teacher {
 
+  String name;
+  int satisfacton;
+
+  public Teacher() {
+    this.satisfacton = 50;
+  }
+
+  public Teacher(String name) {
+    this.name = name;
+    this.satisfacton = 50;
+  }
+
   public String answer(String question) {
     if (question.length() > 0) {
+      satisfacton++;
       return "Yes. Well done!";
     } else {
+      satisfacton--;
+      if (satisfacton == 0) {
+        return "Director should kick you out!";
+      }
       return "It's a stupid question.";
     }
   }
 
   public void teach(Student student) {
     student.learn();
+    satisfacton++;
   }
-
 }
