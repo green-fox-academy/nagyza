2fd7c3ab74c2af42de16ab34d1a553346b07bf65
Zsolt Nagy <nagyza@gmail.com>
2017-04-03 15:01:47 +0200

    Define and set things and load to Fleet type object.

--- week-04/day-01/fleet-of-things/src/FleetOfThings.java
+++ week-04/day-01/fleet-of-things/src/FleetOfThings.java
@@ -7,7 +7,16 @@ public class FleetOfThings {
     // 2. [ ] Remove the obstacles
     // 3. [x] Stand up
     // 4. [x] Eat lunch
-    
+    Thing milk = new Thing("Get milk");
+    Thing obstackles = new Thing("Remove the obstacles");
+    Thing standUp = new Thing("Stand up");
+    standUp.complete();
+    Thing lunch = new Thing("Eat lunch");
+    lunch.complete();
+    fleet.add(milk);
+    fleet.add(obstackles);
+    fleet.add(standUp);
+    fleet.add(lunch);
     System.out.println(fleet);
   }
 }
