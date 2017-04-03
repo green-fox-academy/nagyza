ca7ec59a6cc241248655fc9b76a1c056b2ea90ca
Zsolt Nagy <nagyza@gmail.com>
2017-04-03 15:37:31 +0200

    Create more variable, constructor and method.
    Create getters for thirst and hunger, toString method, name variable. Constructor if name given.

--- week-04/day-01/animal/src/Animal.java
+++ week-04/day-01/animal/src/Animal.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by nagyza on 2017.04.03..
  */
@@ -13,8 +15,15 @@
 
 public class Animal {
 
+  String name;
   int hunger;
   int thirst;
+
+  public Animal(String name) {
+    this.name = name;
+    this.hunger = 50;
+    this.thirst = 50;
+  }
 
   public Animal() {
     this.hunger = 50;
@@ -29,8 +38,36 @@ public class Animal {
     this.thirst -= 1;
   }
 
+  public int getHunger() {
+    return hunger;
+  }
+
+  public int getThirst() {
+    return thirst;
+  }
+
   public void play() {
     this.thirst += 1;
     this.hunger += 1;
   }
+
+  @Override
+  public String toString() {
+    return  "name='" + name + '\'' +
+            ", hunger=" + hunger +
+            ", thirst=" + thirst +
+            '}';
+  }
+
+  public static void main(String[] args) {
+    Animal animal1 = new Animal("Bestia");
+    Animal animal2 = new Animal("Vadállat");
+
+    animal1.eat();
+    System.out.println(animal1.getHunger());
+    animal2.play();
+    System.out.println(animal1);
+    System.out.println(animal2);
+  }
+
 }
