9eab2e2c03adeda6508eb08b3f33ecc56391d89f
Zsolt Nagy <nagyza@gmail.com>
2017-04-05 11:06:52 +0200

    Create test class to test Apple class getApple method.

---
+++ week-04/day-03/apples/tests/ApplesTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplesTest {
+  @Test
+  public void getApple() throws Exception {
+    Apples apples1 = new Apples();
+    assertEquals("apple", apples1.getApple());
+  }
+
+
+}
