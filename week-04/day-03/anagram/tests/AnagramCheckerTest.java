5bad7032cdb2599a732d2fb8e51a1ed281bd8f2e
Zsolt Nagy <nagyza@gmail.com>
2017-04-05 13:54:42 +0200

    Test isAnagram with obvious anagram words

--- week-04/day-03/anagram/tests/AnagramCheckerTest.java
+++ week-04/day-03/anagram/tests/AnagramCheckerTest.java
@@ -6,6 +6,15 @@ public class AnagramCheckerTest {
   @Test
   public void isAnagramEqualStrings() throws Exception {
     AnagramChecker trial = new AnagramChecker();
-    assertTrue(trial.isAnagram("valami", "valami"));
+    String word = "valami";
+    assertTrue(trial.isAnagram(word, word));
+  }
+
+  @Test
+  public void isAnagramAnagramStrings() throws Exception {
+    AnagramChecker trial = new AnagramChecker();
+    String word1 = "valami";
+    String word2 = "vamali";
+    assertTrue(trial.isAnagram(word1, word2));
   }
 }
