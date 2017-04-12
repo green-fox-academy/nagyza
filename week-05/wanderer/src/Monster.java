ed5c24b3d8659a226c1a34d122de3a9286ffcfb0
Zsolt Nagy <nagyza@gmail.com>
2017-04-12 23:05:45 +0200

    Create key variable, setKeyTrue and toString methods

--- week-05/wanderer/src/Monster.java
+++ week-05/wanderer/src/Monster.java
@@ -1,5 +1,7 @@
 public class Monster extends GameCharacter {
-  public Monster(int[] positionXY, String image) {
+  boolean key;
+
+  public Monster(int[] positionXY, String image, boolean key) {
     positionX = positionXY[0];
     positionY = positionXY[1];
     imageName = image;
@@ -9,11 +11,22 @@ public class Monster extends GameCharacter {
       currentHealthPoint = maxHealthPoint;
       defendPoint = (int) Math.ceil((double)level / 2 ) * getDieRoll() + (int)Math.ceil((double)getDieRoll() / 2);
       strikePoint = level * getDieRoll() + level;
+      key = false;
     } else {
       maxHealthPoint = level * 2 * getDieRoll();
       currentHealthPoint = maxHealthPoint;
       defendPoint = (int) Math.ceil((double)level / 2 ) * getDieRoll();
       strikePoint = level * getDieRoll();
+      this.key = key;
     }
+  }
+
+  @Override
+  public String toString() {
+    return "M HP" + maxHealthPoint + ":" + currentHealthPoint + " DP" + defendPoint + " SP" + strikePoint;
+  }
+
+  public void setKeyTrue() {
+    key = true;
   }
 }
