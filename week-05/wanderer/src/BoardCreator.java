0ef601a6b76cc60c412a147d74307daccc38197f
Zsolt Nagy <nagyza@gmail.com>
2017-04-12 13:51:59 +0200

    Create more class variables and randomCoord method, complete constructor

--- week-05/wanderer/src/BoardCreator.java
+++ week-05/wanderer/src/BoardCreator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class BoardCreator {
 
@@ -11,11 +12,23 @@ public class BoardCreator {
           {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
           {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
           {0, 0, 0, 1, 0, 1, 1, 0, 1, 0}};
-
+  int level;
   private Hero gameHero;
+  private Monster skeleton1;
+  private Monster skeleton2;
+  private Monster skeleton3;
+  private Monster boss;
+  private ArrayList<Monster> allMonsters;
 
   public BoardCreator() {
+    this.allMonsters = new ArrayList<>();
     gameHero = new Hero();
+    this.level = 1;
+  }
+
+  private int[] randomCoord() {
+    int[] place = {(int) (Math.random() * 9) + 1, (int) (Math.random() * 9) + 1};
+    return place;
   }
 
   public Hero getGameHero() {
