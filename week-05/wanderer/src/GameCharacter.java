fd20b3a306451ccaefba0c87e929ad9780458a63
Zsolt Nagy <nagyza@gmail.com>
2017-04-12 12:15:49 +0200

    Refactor all possible variables and methods from Hero to GameCharacter

--- week-05/wanderer/src/GameCharacter.java
+++ week-05/wanderer/src/GameCharacter.java
@@ -1,57 +1,48 @@
 
 public class GameCharacter {
-  private int maxHealthPoint;
-  private int currentHealthPoint;
-  private int defendPoint;
-  private int strikePoint;
-  private int dieRoll;
-  private String type;
-  private int level;
-  private int positionX;
-  private int positionY;
-  private String imageName;
-  private PositionedImage imageDraw;
-  public static Board board = new Board();
+  int maxHealthPoint;
+  int currentHealthPoint;
+  int defendPoint;
+  int strikePoint;
+  int level;
+  int dieRoll;
+  int positionX;
+  int positionY;
+  String imageName;
+  PositionedImage imageDraw;
 
-  public GameCharacter(String type, int level, int positionX, int positionY) {
-    this.type = type;
-    this.level = level;
-    if (this.type.equals("hero")) {
-      this.maxHealthPoint = 20 + 3 * getDieRoll();
-      this.currentHealthPoint = this.maxHealthPoint;
-      this.defendPoint = 2 * getDieRoll();
-      this.strikePoint = 5 + getDieRoll();
-      this.imageName = "assets/hero-down.png";
-      this.positionX = positionX;
-      this.positionY = positionY;
-      this.imageDraw = new PositionedImage(this.imageName, this.positionX, this.positionY);
-    } else if (this.type.equals("monster")) {
-      this.maxHealthPoint = 2 * this.level * getDieRoll();
-      this.currentHealthPoint = this.maxHealthPoint;
-      this.defendPoint = this.level * getDieRoll(); // todo It isn't definitive
-      this.strikePoint = this.level + getDieRoll();
-    } // todo create a boss
+  public void moveUp(int[][] tilesOrder) {
+    this.whereToGo("up", tilesOrder);
   }
 
-  public void whereToGo(String direction) {
+  public void moveDown(int[][] tilesOrder) {
+    this.whereToGo("down", tilesOrder);
+  }
+
+  public void moveLeft(int[][] tilesOrder) {
+    this.whereToGo("left", tilesOrder);
+  }
+
+  public void moveRight(int[][] tilesOrder) {
+    this.whereToGo("right", tilesOrder);
+  }
+
+
+  public void whereToGo(String direction, int[][] tilesOrder) {
     if (direction.equals("up")) {
-      this.imageName = "assets/hero-up.png";
-      if (this.positionY > 0 && board.getTilesOrder()[positionY - 1][positionX] != 1) {
+      if (this.positionY > 0 && tilesOrder[positionY - 1][positionX] != 1) {
         this.positionY -= 1;
       }
     } else if (direction.equals("down")) {
-      this.imageName = "assets/hero-down.png";
-      if (this.positionY < 9 && board.getTilesOrder()[positionY + 1][positionX] != 1) {
+      if (this.positionY < 9 && tilesOrder[positionY + 1][positionX] != 1) {
         this.positionY += 1;
       }
     } else if (direction.equals("left")) {
-      this.imageName = "assets/hero-left.png";
-      if (this.positionX > 0 && board.getTilesOrder()[positionY][positionX - 1] != 1) {
+      if (this.positionX > 0 && tilesOrder[positionY][positionX - 1] != 1) {
         this.positionX -= 1;
       }
     } else if (direction.equals("right")) {
-      this.imageName = "assets/hero-right.png";
-      if (this.positionX < 9 && board.getTilesOrder()[positionY][positionX + 1] != 1) {
+      if (this.positionX < 9 && tilesOrder[positionY][positionX + 1] != 1) {
         this.positionX += 1;
       }
     }
--- week-05/wanderer/src/Hero.java
+++ week-05/wanderer/src/Hero.java
@@ -1,15 +1,5 @@
 
-public class Hero {
-  private int maxHealthPoint;
-  private int currentHealthPoint;
-  private int defendPoint;
-  private int strikePoint;
-  private int dieRoll;
-  private int level;
-  private int positionX;
-  private int positionY;
-  private String imageName;
-  private PositionedImage imageDraw;
+public class Hero extends GameCharacter {
 
   public Hero() {
     this.level = 1;
@@ -23,52 +13,27 @@ public class Hero {
     this.imageDraw = new PositionedImage(this.imageName, this.positionX, this.positionY);
   }
 
+  @Override
   public void moveUp(int[][] tilesOrder) {
     this.imageName = "assets/hero-up.png";
     this.whereToGo("up", tilesOrder);
   }
 
+  @Override
   public void moveDown(int[][] tilesOrder) {
     this.imageName = "assets/hero-down.png";
     this.whereToGo("down", tilesOrder);
   }
 
+  @Override
   public void moveLeft(int[][] tilesOrder) {
     this.imageName = "assets/hero-left.png";
     this.whereToGo("left", tilesOrder);
   }
 
+  @Override
   public void moveRight(int[][] tilesOrder) {
     this.imageName = "assets/hero-right.png";
     this.whereToGo("right", tilesOrder);
-  }
-
-  public void whereToGo(String direction, int[][] tilesOrder) {
-    if (direction.equals("up")) {
-      if (this.positionY > 0 && tilesOrder[positionY - 1][positionX] != 1) {
-        this.positionY -= 1;
-      }
-    } else if (direction.equals("down")) {
-      if (this.positionY < 9 && tilesOrder[positionY + 1][positionX] != 1) {
-        this.positionY += 1;
-      }
-    } else if (direction.equals("left")) {
-      if (this.positionX > 0 && tilesOrder[positionY][positionX - 1] != 1) {
-        this.positionX -= 1;
-      }
-    } else if (direction.equals("right")) {
-      if (this.positionX < 9 && tilesOrder[positionY][positionX + 1] != 1) {
-        this.positionX += 1;
-      }
-    }
-  }
-
-  PositionedImage getImageDraw() {
-    return new PositionedImage(this.imageName, this.positionX, this.positionY);
-  }
-
-  int getDieRoll() {
-    this.dieRoll = (int) (Math.random() * 6) + 1;
-    return this.dieRoll;
   }
 }
