7cbc4ae656b78a2bc055c2dd959e893d16990386
Zsolt Nagy <nagyza@gmail.com>
2017-04-19 15:44:30 +0200

    Fix rank value at ACE

--- week-06/day-02/twenty-plus-one/src/CardRank.java
+++ week-06/day-02/twenty-plus-one/src/CardRank.java
@@ -1,6 +1,6 @@
 
 public enum  CardRank {
-  C2(2), C3(3), C4(4), C5(5), C6(6), C7(7), C8(8), C9(9), C10(10), JUMBO(10), DAMA(10), QUEEN(10), ACE(10);
+  C2(2), C3(3), C4(4), C5(5), C6(6), C7(7), C8(8), C9(9), C10(10), JUMBO(10), DAMA(10), QUEEN(10), ACE(11);
 
   private int value;
   private static final int SIZE = values().length;
