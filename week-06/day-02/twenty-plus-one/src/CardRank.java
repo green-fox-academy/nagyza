ebb60a4b867c109ab4e21baf942314abf6f8b600
Zsolt Nagy <nagyza@gmail.com>
2017-04-19 12:21:59 +0200

    Create getValue and getRandomRank methods

--- week-06/day-02/twenty-plus-one/src/CardRank.java
+++ week-06/day-02/twenty-plus-one/src/CardRank.java
@@ -1,10 +1,18 @@
 
 public enum  CardRank {
-  C2(2), C3(3), C4(4), C5(5), C6(6), C7(7), C8(8), C9(9), C10(10), J(10), Q(10), K(10), A(10);
+  C2(2), C3(3), C4(4), C5(5), C6(6), C7(7), C8(8), C9(9), C10(10), JUMBO(10), DAMA(10), QUEEN(10), ACE(10);
 
   private int value;
 
-  private CardRank(int value) {
+  CardRank(int value) {
     this.value = value;
+  }
+
+  public static CardRank getRandomRank() {
+    return values()[(int) (Math.random() * 13)];
+  }
+
+  public int getValue() {
+    return value;
   }
 }
