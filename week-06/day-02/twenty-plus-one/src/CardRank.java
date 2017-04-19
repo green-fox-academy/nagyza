12b652c9cea66ee5f0f3c82e2452326c9a00359a
Zsolt Nagy <nagyza@gmail.com>
2017-04-19 13:28:53 +0200

    Declare SIZE variable for number of values

--- week-06/day-02/twenty-plus-one/src/CardRank.java
+++ week-06/day-02/twenty-plus-one/src/CardRank.java
@@ -3,13 +3,14 @@ public enum  CardRank {
   C2(2), C3(3), C4(4), C5(5), C6(6), C7(7), C8(8), C9(9), C10(10), JUMBO(10), DAMA(10), QUEEN(10), ACE(10);
 
   private int value;
+  private static final int SIZE = values().length;
 
   CardRank(int value) {
     this.value = value;
   }
 
   public static CardRank getRandomRank() {
-    return values()[(int) (Math.random() * 13)];
+    return values()[(int) (Math.random() * SIZE)];
   }
 
   public int getValue() {
