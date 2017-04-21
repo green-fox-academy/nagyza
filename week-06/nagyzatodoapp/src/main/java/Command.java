96d73b7c49a216bcfd0669741c3672110963adee
Zsolt Nagy <nagyza@gmail.com>
2017-04-21 13:26:16 +0200

    Create abstract class for possible commands

---
+++ week-06/nagyzatodoapp/src/main/java/Command.java
@@ -0,0 +1,26 @@
+
+public abstract class Command {
+  private String argument;
+  private String commandDescription;
+  private String detailedHelp;
+
+  public Command(String argument, String commandDescription, String detailedHelp) {
+    this.argument = argument;
+    this.commandDescription = commandDescription;
+    this.detailedHelp = detailedHelp;
+  }
+
+  public abstract void execute();
+
+  public String getArgument() {
+    return argument;
+  }
+
+  public String getCommandDescription() {
+    return commandDescription;
+  }
+
+  public String getDetailedHelp() {
+    return detailedHelp;
+  }
+}
