dd6e496a43e9f5e6916eb959b44f96410b70da81
Zsolt Nagy <nagyza@gmail.com>
2017-05-02 13:19:23 +0200

    Add new AtomicLong type object to count calls of Greeting

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
-
+  AtomicLong counter = new AtomicLong(1);
   @RequestMapping
   public Greeting greeting(@RequestParam("name") String name) {
-    return new Greeting(1, "Hello " + name);
+    return new Greeting(counter.getAndIncrement(), "Hello " + name);
   }
 }
