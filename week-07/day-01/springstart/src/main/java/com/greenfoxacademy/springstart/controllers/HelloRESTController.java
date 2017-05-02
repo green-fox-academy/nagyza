950cdebf23f6ea97e2decf9616718cfeeb86a3b3
Zsolt Nagy <nagyza@gmail.com>
2017-05-02 12:21:29 +0200

    Modify method to use RequestParam annotation

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @RequestMapping
-  public Greeting greeting() {
-    return new Greeting(1, "Hello, World! Hello, hello!");
+  public Greeting greeting(@RequestParam("name") String name) {
+    return new Greeting(1, name);
   }
 }
