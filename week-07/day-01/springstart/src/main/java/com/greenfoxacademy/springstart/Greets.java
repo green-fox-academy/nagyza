a51e22c81ac58e321c5d41d9fbf85bad0465b167
Zsolt Nagy <nagyza@gmail.com>
2017-05-02 16:17:22 +0200

    Create new class to make possible to use each loop in the template

---
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/Greets.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.springstart;
+
+public class Greets {
+  public String greet;
+
+  public Greets(String greet) {
+    this.greet = greet;
+  }
+
+  public String getGreet() {
+    return greet;
+  }
+}
--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,19 +1,31 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import com.greenfoxacademy.springstart.Greets;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
   AtomicLong counter = new AtomicLong(1);
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam("name") String name) {
+    List<Greets> greets = new ArrayList<>();
+    for (String hello : hellos) {
+      greets.add(new Greets(hello));
+    }
     model.addAttribute("name", name);
     model.addAttribute("counter", counter.getAndIncrement());
+    model.addAttribute("hellos", greets);
     return "greeting";
   }
 }
