1055d7073f5e6ed6efde4b7e4b085db45c4ae4a3
Zsolt Nagy <nagyza@gmail.com>
2017-05-02 11:22:31 +0200

    Create hello method

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
@@ -2,9 +2,19 @@ package com.greenfoxacademy.springstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class SpringstartApplication {
+
+  @RequestMapping(value="/hello")
+  @ResponseBody
+  public String hello() {
+    return "Hello World! Greetings from n@gyza!";
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstartApplication.class, args);
