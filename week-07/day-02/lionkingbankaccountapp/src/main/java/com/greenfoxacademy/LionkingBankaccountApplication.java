9cd52ef492982e42cedc2de386d309161b43cbb4
Zsolt Nagy <nagyza@gmail.com>
2017-05-03 11:50:07 +0200

    Fix name of class

--- week-07/day-02/lionkingbankaccountapp/src/main/java/com/greenfoxacademy/LionkingBankaccountApplication.java
+++ week-07/day-02/lionkingbankaccountapp/src/main/java/com/greenfoxacademy/LionkingBankaccountApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LionkingbankaccountappApplication {
+public class LionkingBankaccountApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LionkingbankaccountappApplication.class, args);
+		SpringApplication.run(LionkingBankaccountApplication.class, args);
 	}
 }
