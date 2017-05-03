f2292578e44d8bf0cd5751190e98df460fc40f42
Zsolt Nagy <nagyza@gmail.com>
2017-05-03 12:41:41 +0200

    Create constructor and getters

--- week-07/day-02/lionkingbankaccountapp/src/main/java/com/greenfoxacademy/BankAccount.java
+++ week-07/day-02/lionkingbankaccountapp/src/main/java/com/greenfoxacademy/BankAccount.java
@@ -2,7 +2,24 @@ package com.greenfoxacademy;
 
 public class BankAccount {
   private String name;
-  private int balance;
+  private long balance;
   private String animalType;
 
+  public BankAccount(String name, long balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public long getBalance() {
+    return balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
 }
