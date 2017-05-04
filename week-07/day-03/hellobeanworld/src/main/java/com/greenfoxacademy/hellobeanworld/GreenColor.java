4f9a2c4ae485b32d9b39f379aa62844c8980b2a4
Zsolt Nagy <nagyza@gmail.com>
2017-05-04 12:07:49 +0200

    Create class which implements MyColor interface

---
+++ week-07/day-03/hellobeanworld/src/main/java/com/greenfoxacademy/hellobeanworld/GreenColor.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.hellobeanworld;
+
+public class GreenColor implements MyColor {
+
+  @Override
+  public void printColor() {
+    System.out.println("It is green in color...");
+  }
+}
