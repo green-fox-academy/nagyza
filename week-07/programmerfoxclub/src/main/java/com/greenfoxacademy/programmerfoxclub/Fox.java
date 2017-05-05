e5370dc009f5323dab557b12e30a2231875651cf
Zsolt Nagy <nagyza@gmail.com>
2017-05-05 13:23:49 +0200

    Create getters and setters for Fox class

--- week-07/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/Fox.java
+++ week-07/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/Fox.java
@@ -8,13 +8,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Getter
-@Setter
 public class Fox {
-  public String name;
-  public String food;
-  public String drink;
-  public List<String> tricks = new ArrayList<>();
+  private String name;
+  private String food;
+  private String drink;
+  private List<String> tricks = new ArrayList<>();
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public void setFood(String food) {
+    this.food = food;
+  }
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public void setDrink(String drink) {
+    this.drink = drink;
+  }
+
+  public List<String> getTricks() {
+    return tricks;
+  }
+
+  public void setTricks(List<String> tricks) {
+    this.tricks = tricks;
+  }
 
   public void addTrick(String trick) {
     tricks.add(trick);
