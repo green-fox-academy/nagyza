eda60c1f7ed34336cf3e63bcaa66364ec3b0118d
Zsolt Nagy <nagyza@gmail.com>
2017-05-09 13:37:18 +0200

    Put into repository package

--- week-08/day-02/listingtodoswithh2/src/main/java/com/greenfoxacademy/listingtodoswithh2/repository/TodoRepository.java
+++ week-08/day-02/listingtodoswithh2/src/main/java/com/greenfoxacademy/listingtodoswithh2/repository/TodoRepository.java
@@ -1,5 +1,6 @@
-package com.greenfoxacademy.listingtodoswithh2.model;
+package com.greenfoxacademy.listingtodoswithh2.repository;
 
+import com.greenfoxacademy.listingtodoswithh2.model.Todo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
