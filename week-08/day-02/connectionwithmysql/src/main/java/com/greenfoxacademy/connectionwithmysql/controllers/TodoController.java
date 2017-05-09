f413ea0d38c81f32d312b051bd8a5751f1a33b73
Zsolt Nagy <nagyza@gmail.com>
2017-05-09 15:21:29 +0200

    Create classes, interface and templates, prepare database and application.properties

---
+++ week-08/day-02/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/TodoController.java
@@ -0,0 +1,34 @@
+package com.greenfoxacademy.connectionwithmysql.controllers;
+
+import com.greenfoxacademy.connectionwithmysql.model.Todo;
+import com.greenfoxacademy.connectionwithmysql.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TodoController {
+
+  @Autowired
+  TodoRepository todoRepository;
+
+  @RequestMapping({"/", "/list"})
+  public String index(Model model) {
+    return "index";
+  }
+
+  @RequestMapping("/todo")
+  public String list(Model model) {
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todo";
+  }
+
+  @RequestMapping("/addTodo")
+  public String addTodo(Model model, @RequestParam("title") String param) {
+    todoRepository.save(new Todo(param));
+    model.addAttribute("notes", todoRepository.findAll());
+    return "redirect:/todo";
+  }
+}
