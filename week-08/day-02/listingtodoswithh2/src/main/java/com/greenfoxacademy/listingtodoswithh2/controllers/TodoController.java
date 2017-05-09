d876dced6eae6ef468f3471ce45748496c363ac9
Zsolt Nagy <nagyza@gmail.com>
2017-05-09 14:06:23 +0200

    Create addTodo endpoint with preapared template

--- week-08/day-02/listingtodoswithh2/src/main/java/com/greenfoxacademy/listingtodoswithh2/controllers/TodoController.java
+++ week-08/day-02/listingtodoswithh2/src/main/java/com/greenfoxacademy/listingtodoswithh2/controllers/TodoController.java
@@ -1,10 +1,12 @@
 package com.greenfoxacademy.listingtodoswithh2.controllers;
 
+import com.greenfoxacademy.listingtodoswithh2.model.Todo;
 import com.greenfoxacademy.listingtodoswithh2.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -19,6 +21,14 @@ public class TodoController {
 
   @RequestMapping("/todo")
   public String list(Model model) {
+    model.addAttribute("notes", todoRepository.findAll());
     return "todo";
+  }
+
+  @RequestMapping("/addTodo")
+  public String addTodo(Model model, @RequestParam("title") String param) {
+    todoRepository.save(new Todo(param));
+    model.addAttribute("notes", todoRepository.findAll());
+    return "redirect:/todo";
   }
 }
