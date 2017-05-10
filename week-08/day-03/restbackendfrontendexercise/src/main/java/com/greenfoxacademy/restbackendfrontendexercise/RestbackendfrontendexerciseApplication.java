0c8ca94a5ca2f094178eb01f62bb175026d09bc7
Zsolt Nagy <nagyza@gmail.com>
2017-05-10 12:27:32 +0200

    Create class in initialization process

---
+++ week-08/day-03/restbackendfrontendexercise/src/main/java/com/greenfoxacademy/restbackendfrontendexercise/RestbackendfrontendexerciseApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.restbackendfrontendexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbackendfrontendexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbackendfrontendexerciseApplication.class, args);
+	}
+}
