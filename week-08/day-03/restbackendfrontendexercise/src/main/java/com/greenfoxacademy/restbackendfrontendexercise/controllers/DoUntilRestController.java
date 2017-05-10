787839dca342f162a52fe1b5116d2c0f89abea87
Zsolt Nagy <nagyza@gmail.com>
2017-05-10 17:05:51 +0200

    Put RequestBody variable into endpoint method

--- week-08/day-03/restbackendfrontendexercise/src/main/java/com/greenfoxacademy/restbackendfrontendexercise/controllers/DoUntilRestController.java
+++ week-08/day-03/restbackendfrontendexercise/src/main/java/com/greenfoxacademy/restbackendfrontendexercise/controllers/DoUntilRestController.java
@@ -1,14 +1,22 @@
 package com.greenfoxacademy.restbackendfrontendexercise.controllers;
 
+import com.greenfoxacademy.restbackendfrontendexercise.model.DoUntil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DoUntilRestController {
 
+  @Autowired
+  DoUntil doUntil;
+
   @PostMapping("/dountil/{what}")
-  public String doUntilNum(@PathVariable("what") String what) {
-    return what;
+  public DoUntil doUntilNum(@PathVariable("what") String what, @RequestBody DoUntil until) {
+    doUntil = until;
+    doUntil.setWhat(what);
+    return doUntil;
   }
 }
