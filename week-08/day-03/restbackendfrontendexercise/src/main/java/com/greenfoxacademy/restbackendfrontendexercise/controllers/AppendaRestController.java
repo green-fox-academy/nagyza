1932cbe46d7a035176ae1dd1d6c47c7ef6521679
Zsolt Nagy <nagyza@gmail.com>
2017-05-10 15:12:11 +0200

    Complete endpoint and create corresponding class

--- week-08/day-03/restbackendfrontendexercise/src/main/java/com/greenfoxacademy/restbackendfrontendexercise/controllers/AppendaRestController.java
+++ week-08/day-03/restbackendfrontendexercise/src/main/java/com/greenfoxacademy/restbackendfrontendexercise/controllers/AppendaRestController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.restbackendfrontendexercise.controllers;
 
+import com.greenfoxacademy.restbackendfrontendexercise.model.AppendA;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppendaRestController {
 
+  @Autowired
+  AppendA appendA;
 
   @GetMapping("/appenda/{appendable}")
-  public String getApendA(@PathVariable("appendable") String appendTo) {
-    return appendTo + "a";
+  public AppendA getAppendA(@PathVariable("appendable") String appendTo) {
+    appendA.setAppended(appendTo);
+    return appendA;
   }
-
 }
