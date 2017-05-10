c11441d68c5228900deee7a867f825fb5a593311
Zsolt Nagy <nagyza@gmail.com>
2017-05-10 13:21:28 +0200

    Change Component annotation to Service

--- week-08/day-03/restbackendfrontendexercise/src/main/java/com/greenfoxacademy/restbackendfrontendexercise/model/Doubling.java
+++ week-08/day-03/restbackendfrontendexercise/src/main/java/com/greenfoxacademy/restbackendfrontendexercise/model/Doubling.java
@@ -1,8 +1,8 @@
 package com.greenfoxacademy.restbackendfrontendexercise.model;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class Doubling {
   private int received;
   private int result;
