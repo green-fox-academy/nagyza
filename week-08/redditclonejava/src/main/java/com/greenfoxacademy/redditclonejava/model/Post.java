3b873a78f7339687617fee51efa6c6d44280de49
Zsolt Nagy <nagyza@gmail.com>
2017-05-12 11:04:50 +0200

    Create class based on the API spec

---
+++ week-08/redditclonejava/src/main/java/com/greenfoxacademy/redditclonejava/model/Post.java
@@ -0,0 +1,57 @@
+package com.greenfoxacademy.redditclonejava.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  private String title;
+  private String href;
+  private long timestamp;
+  private int score;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getHref() {
+    return href;
+  }
+
+  public void setHref(String href) {
+    this.href = href;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+}
