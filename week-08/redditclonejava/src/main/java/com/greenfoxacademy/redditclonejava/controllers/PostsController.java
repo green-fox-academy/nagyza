0613355a129454ac96c60985fbd9aceb4deeacc6
Zsolt Nagy <nagyza@gmail.com>
2017-05-12 16:04:20 +0200

    Create PostMapping endpoint

--- week-08/redditclonejava/src/main/java/com/greenfoxacademy/redditclonejava/controllers/PostsController.java
+++ week-08/redditclonejava/src/main/java/com/greenfoxacademy/redditclonejava/controllers/PostsController.java
@@ -1,9 +1,12 @@
 package com.greenfoxacademy.redditclonejava.controllers;
 
+import com.greenfoxacademy.redditclonejava.model.Post;
 import com.greenfoxacademy.redditclonejava.model.Posts;
 import com.greenfoxacademy.redditclonejava.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,10 +16,18 @@ public class PostsController {
   PostRepository postRepository;
 
   Posts posts = new Posts();
-
   @GetMapping("/posts")
   public Posts getPost() {
     posts.setPosts(postRepository.findAll());
     return posts;
+  }
+
+  @PostMapping("/posts")
+  public Post postPost(@RequestBody Post postIn) {
+    Post post = new Post();
+    post.setTitle(postIn.getTitle());
+    post.setHref(postIn.getHref());
+    postRepository.save(post);
+    return post;
   }
 }
