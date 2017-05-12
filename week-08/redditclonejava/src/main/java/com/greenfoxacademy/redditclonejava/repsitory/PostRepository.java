e482f5f539b9f7a5c80feafb879d7a3e081b55b7
Zsolt Nagy <nagyza@gmail.com>
2017-05-12 11:46:20 +0200

    Fix interface declaration

--- week-08/redditclonejava/src/main/java/com/greenfoxacademy/redditclonejava/repsitory/PostRepository.java
+++ week-08/redditclonejava/src/main/java/com/greenfoxacademy/redditclonejava/repsitory/PostRepository.java
@@ -1,8 +1,9 @@
 package com.greenfoxacademy.redditclonejava.repsitory;
 
+import com.greenfoxacademy.redditclonejava.model.Post;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface PostRepository extends CrudRepository {
+public interface PostRepository extends CrudRepository<Post, Long> {
 }
