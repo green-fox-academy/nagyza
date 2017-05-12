421dfbdcb4d34153c46f2da2aae8360b12eef123
Zsolt Nagy <nagyza@gmail.com>
2017-05-12 11:31:20 +0200

    Create interface as extended CrudRepository

---
+++ week-08/redditclonejava/src/main/java/com/greenfoxacademy/redditclonejava/repsitory/PostRepository.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.redditclonejava.repsitory;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface PostRepository extends CrudRepository {
+}
