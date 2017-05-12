6233eb35c2a14725599215133a9e09a2ed1b2385
Zsolt Nagy <nagyza@gmail.com>
2017-05-12 11:03:57 +0200

    Initialize project

---
+++ week-08/redditclonejava/src/main/java/com/greenfoxacademy/redditclonejava/RedditclonejavaApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.redditclonejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditclonejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditclonejavaApplication.class, args);
+	}
+}
---
+++ week-08/redditclonejava/src/main/java/com/greenfoxacademy/redditclonejava/controllers/MainController.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.redditclonejava.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+
+
+}
