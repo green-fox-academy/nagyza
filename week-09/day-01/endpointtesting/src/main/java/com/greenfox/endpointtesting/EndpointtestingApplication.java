e76bc6f6a0877ac512e4b4043b430e6f2cd83637
Zsolt Nagy <nagyza@gmail.com>
2017-05-15 11:51:57 +0200

    Initialize project

---
+++ week-09/day-01/endpointtesting/src/main/java/com/greenfox/endpointtesting/EndpointtestingApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.endpointtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointtestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointtestingApplication.class, args);
+	}
+}
