560ee8fa62afbd8e7d0bca391505bef6f9d06456
Zsolt Nagy <nagyza@gmail.com>
2017-05-15 11:55:28 +0200

    Create controller with ExceptionHandler and an endpoint

---
+++ week-09/day-01/endpointtesting/src/main/java/com/greenfox/endpointtesting/controllers/GuardianController.java
@@ -0,0 +1,29 @@
+package com.greenfox.endpointtesting.controllers;
+
+import com.greenfox.endpointtesting.services.ErrorMessage;
+import com.greenfox.endpointtesting.services.Guardian;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GuardianController {
+
+  @Autowired
+  ErrorMessage errorMessage;
+
+  @RequestMapping(value = "/groot", method = RequestMethod.GET)
+  public Guardian grootResponse(@RequestParam(value = "message") String message) {
+    Guardian guardian = new Guardian();
+    guardian.setMessage(message);
+    guardian.setTranslated("I am Groot!");
+    return guardian;
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ErrorMessage errorMessageSender(MissingServletRequestParameterException e) {
+    String paramName = e.getParameterName();
+    errorMessage.setError("I am Groot!");
+    return errorMessage;
+  }
+}
