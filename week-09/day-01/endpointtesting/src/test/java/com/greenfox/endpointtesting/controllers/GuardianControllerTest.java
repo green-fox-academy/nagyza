fe79dd9b68ffddc27dd2c63a7e238a7463e78d9b
Zsolt Nagy <nagyza@gmail.com>
2017-05-15 13:58:33 +0200

    Create test class for integration test with a test case with parameter

--- week-09/day-01/endpointtesting/src/test/java/com/greenfox/endpointtesting/controllers/GuardianControllerTest.java
+++ week-09/day-01/endpointtesting/src/test/java/com/greenfox/endpointtesting/controllers/GuardianControllerTest.java
@@ -9,9 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
@@ -31,7 +34,12 @@ public class GuardianControllerTest {
   }
 
   @Test
-  public void grootResponse() throws Exception {
+  public void grootResponseWithRequestParam() throws Exception {
+
+    mockMvc.perform(get("/groot?message=somemessage"))
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.received").value("somemessage"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.translated").value("I am Groot!"));
 
   }
 
