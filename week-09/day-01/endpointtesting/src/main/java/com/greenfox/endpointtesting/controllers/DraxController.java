da192e28cd5612ecfe8a98cca266e47b8a3bd40c
Zsolt Nagy <nagyza@gmail.com>
2017-05-15 22:02:57 +0200

    Add new endpoint to handle post request

--- week-09/day-01/endpointtesting/src/main/java/com/greenfox/endpointtesting/controllers/DraxController.java
+++ week-09/day-01/endpointtesting/src/main/java/com/greenfox/endpointtesting/controllers/DraxController.java
@@ -4,8 +4,7 @@ import com.greenfox.endpointtesting.repository.DraxRepository;
 import com.greenfox.endpointtesting.services.CalorieTable;
 import com.greenfox.endpointtesting.services.Food;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DraxController {
@@ -25,6 +24,12 @@ public class DraxController {
     return calorieTable;
   }
 
+  @PostMapping("/addfood")
+  public Food addFoodToCalorieTable(@RequestBody Food food) {
+    this.food = food;
+    draxRepository.save(this.food);
+    return food;
+  }
 
 
 }
