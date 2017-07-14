36c4d85c5d2087aeaebb554c10e36684948594a2
Zsolt Nagy <nagyza@gmail.com>
2017-07-14 11:45:49 +0200

    C# workshop excercise-01

---
+++ week-15/Excercise-01/Excercise-01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            IEnumerable<int> evens = from ns in n where ns % 2 == 0 select ns;
+            foreach (int ens in evens)
+                {
+                    Console.WriteLine(ens);
+                }
+            Console.ReadKey();
+        }
+    }
+}
