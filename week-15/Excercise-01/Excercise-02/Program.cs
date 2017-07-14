50ea25944a39c93065282630e473a0b31570140d
Zsolt Nagy <nagyza@gmail.com>
2017-07-14 13:52:43 +0200

    Excercise-02, fix excercise-01

--- week-15/Excercise-01/Excercise-02/Program.cs
+++ week-15/Excercise-01/Excercise-02/Program.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Excercise_01
+namespace Excercise_02
 {
     class Program
     {
         static void Main(string[] args)
         {
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
-            IEnumerable<int> evens = from ns in n where ns % 2 == 0 select ns;
-            foreach (int ens in evens)
+            IEnumerable<int> odds = from ns in n where ns % 2 != 0 select ns;
+            foreach (int odd in odds)
             {
-                Console.WriteLine(ens);
+                Console.WriteLine(odd);
             }
             Console.ReadKey();
         }
