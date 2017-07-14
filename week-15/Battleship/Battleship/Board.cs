05b0f75328ad62b3a797830e9fe89cdf38b4a418
Zsolt Nagy <nagyza@gmail.com>
2017-07-14 16:05:26 +0200

    Create class, print out battlefield

---
+++ week-15/Battleship/Battleship/Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Board
+    {
+        private int[] _BattleFieldRow = new int[10];
+        private List<int[]> _BattleField = new List<int[]>();
+
+        public int[] BattleFieldRow { get => _BattleFieldRow; set => _BattleFieldRow = value; }
+        public List<int[]> BattleField { get => _BattleField; set => _BattleField = value; }
+    }
+}
---
+++ week-15/Battleship/Battleship/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Program
+    {
+        public static Board gameBoard = new Board();
+        static void Main(string[] args)
+        {
+            int[] row = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                row[i] = 0;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                gameBoard.BattleField.Add(row);
+            }
+
+            foreach (int[] i in gameBoard.BattleField)
+            {
+                foreach (int j in i)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
